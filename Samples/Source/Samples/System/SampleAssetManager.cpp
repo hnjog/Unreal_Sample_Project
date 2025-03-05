acbba77fcc11f17305dd36bf7e45182cd3b823bc
Hyun <hnjogo@gmail.com>
2025-03-05 20:33:14 +0900

    asset manager 만들기
    1. 라이브 코딩 : (ctrl + alt + f11)
    2. System 등 중복 이름이 될 수 폴더 및 파일인 경우,
        경로를 확실히 표현하기
    3. 라이브 코딩에서 빌드에 실패한 경우 MSB3073 발생 가능

---
+++ Samples/Source/Samples/System/SampleAssetManager.cpp
@@ -0,0 +1,2 @@
+﻿#include "../System/SampleAssetManager.h"
+// 민감한 폴더 이름을 사용하는 경우는 경로를 확실히 하자
---
+++ Samples/Source/Samples/System/SampleAssetManager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "SampleAssetManager.generated.h"
+
+UCLASS()
+class SAMPLES_API USampleAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
