056261da1b1ed28c1efb14c5bf777193a986ff91
Hyun <hnjogo@gmail.com>
2025-03-06 19:58:54 +0900

    AssetManger 클래스 생성
    -> 상속받아 일부 기능 추가 및 로깅

--- Samples/Source/Samples/System/SampleAssetManager.cpp
+++ Samples/Source/Samples/System/SampleAssetManager.cpp
@@ -1,2 +1,98 @@
-﻿#include "../System/SampleAssetManager.h"
-// 민감한 폴더 이름을 사용하는 경우는 경로를 확실히 하자
+﻿// 민감한 폴더 이름을 사용하는 경우는 경로를 확실히 하자
+#include "SampleAssetManager.h"
+#include "Samples/SampleLogChannels.h"
+
+USampleAssetManager::USampleAssetManager()
+{
+}
+
+// 에셋 매니저 : singleton
+// -> 유일성을 보장하는 패턴
+USampleAssetManager& USampleAssetManager::Get()
+{
+	check(GEngine);
+
+	// 이 클래스가 오버라이드 된 클래스이기에, GEngine에 AssetManager가 존재
+	if (USampleAssetManager* Singleton = Cast<USampleAssetManager>(GEngine->AssetManager))
+	{
+		return *Singleton;
+	}
+
+	UE_LOG(LogSample, Fatal, TEXT("invalid AssetManagerClassname in DefaultEngine.ini(project settings); it must be SampleAssetManager"));
+
+	// crash가 나기에 도달할 수 없는 상황이지만 컴파일을 위해
+	return *NewObject<USampleAssetManager>();
+}
+
+PRAGMA_DISABLE_OPTIMIZATION
+void USampleAssetManager::StartInitialLoading()
+{
+	// 생성 시점 : 에디터가 켜지기 전 시점 -> Thread-safe 하지 않음
+	Super::StartInitialLoading();
+}
+PRAGMA_ENABLE_OPTIMIZATION
+
+bool USampleAssetManager::ShouldLogAssetLoads()
+{
+	// commandLine을 통하여 command의 인자를 가져올 수 있음
+	// 언리얼 엔진에서도 - 이후 인자를 입력할 수 있고, FCommandLine을 통해 그 인자를 가져올 수 있음
+	const TCHAR* CommandLineContent = FCommandLine::Get();
+	static bool bLogAssetLoads = FParse::Param(CommandLineContent, TEXT("LogAssetLoads"));
+	return bLogAssetLoads;
+}
+
+// synchronous load asset을 불필요하게 호출하는 경우를 확인하기 위함
+// (동기 로딩이 호출되면 게임이 끊기거나 프레임 드랍이 발생하므로 그러한 경우를 체크하기 위한 용)
+// 동기식 에셋 로드
+// 동기 : 완전히 끝날때까지 기다린 후, 다음 작업 진행 (게임 시작 시, 대량 로딩) -> 빠름, 직관적
+// 비동기 : 일정 시간 진행 후, 다른 스레드 등을 통하여 타 작업을 진행 (인게임 중 로딩) -> 유저 경험
+UObject* USampleAssetManager::SynchronousLoadAsset(const FSoftObjectPath& AssetPath)
+{
+	// FSoftObjectPath : 실제로 들고 있는 것이 아닌, 해당 에셋의 파일 경로를 들고 있는 용도
+
+	if (AssetPath.IsValid())
+	{
+		//FScopeLogTime 확인
+		TUniquePtr<FScopeLogTime> LogTimePtr;
+		if (ShouldLogAssetLoads())
+		{
+			// 단순히 로깅하면서, 초단위로 로깅 진행
+			// 동기 로딩의 시간 체크
+			LogTimePtr = MakeUnique<FScopeLogTime>(*FString::Printf(TEXT("synchronous loaded assets [%s]"), *AssetPath.ToString()), nullptr, FScopeLogTime::ScopeLog_Seconds);
+		}
+
+		// 아래의 두 함수는 정말로 로딩이 안되어 있는 경우에만 동적 로딩이 호출됨
+		// 캐싱이 되어있다면 그걸 그대로 가져옴
+
+		// 여기서 두가지 분기:
+		// 1. AssetManager가 있으면, AssetManager의 StreamableManager를 통해 정적 로딩
+		// 2. 아니면, FSoftObjectPath를 통해 바로 정적 로딩
+		if (UAssetManager::IsValid())
+		{
+			// 진짜 로딩을 진행
+			return UAssetManager::GetStreamableManager().LoadSynchronous(AssetPath);
+		}
+
+		// if asset manager is not ready, use LoadObject()
+		// - 슥 보면, StaticLoadObject가 보인다: 
+		// - 참고로, 항상 StaticLoadObject하기 전에 StaticFindObject를 통해 확인하고 실패하면 진짜 로딩함
+		// 매우 느린 로딩 방식(어쩔수 없는 경우에만 사용)
+		return AssetPath.TryLoad();
+	}
+	return nullptr;
+}
+
+void USampleAssetManager::AddLoadedAsset(const UObject* Asset)
+{
+	// 주어진 조건이 항상 참인지 확인할때 사용하는 매크로 (디버깅 모드)
+	// Asset null 체크
+	if (ensureAlways(Asset))
+	{
+		// 임계영역 잠금 관리용 클래스
+		// 생성될때 lock 획득, 범위 종료 시 잠금 해제 (Thread-Safe)
+		// SyncObject가 임계영역이므로, 그것에 Lock을 걸고
+		// 안전하게 Set에 Add
+		FScopeLock Lock(&SyncObject);
+		LoadedAssets.Add(Asset);
+	}
+}
--- Samples/Source/Samples/System/SampleAssetManager.h
+++ Samples/Source/Samples/System/SampleAssetManager.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/AssetManager.h"
@@ -8,5 +8,114 @@ UCLASS()
 class SAMPLES_API USampleAssetManager : public UAssetManager
 {
 	GENERATED_BODY()
-	
+public:
+	USampleAssetManager();
+
+	static USampleAssetManager& Get();
+
+	virtual void StartInitialLoading() final;
+
+	// FScopeLogTime의 사용 여부 판단을 위한 함수
+	static bool ShouldLogAssetLoads();
+
+	static UObject* SynchronousLoadAsset(const FSoftObjectPath& AssetPath);
+
+	// GetAsset : 정적 로딩으로 BP Class 와 Object를 로딩한다
+
+	template<typename AssetType>
+	static AssetType* GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	template<typename AssetType>
+	static TSubclassOf<AssetType> GetSubclass(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	// [Thread-Safe] 메모리에 로딩된 에셋 캐싱
+	void AddLoadedAsset(const UObject* Asset);
+
+	// GC의 대상
+	UPROPERTY()
+	TSet<TObjectPtr<const UObject>> LoadedAssets; // 캐싱을 위한 일종의 장치 (여기 담아둠으로서 메모리에 상주시킴)
+
+	// Object 단위의 Locking
+	FCriticalSection SyncObject;
 };
+
+// Asset : 게임 내의 모든 리소스 (UObject 상속)
+// 완성된 '객체'를 저장하기 위한 단위
+// 
+// staticclass : 메타데이터(리플렉션)
+// 특정한 클래스의 UClass를 반환하는 메서드 (클래스의 메타 데이터에 접근하는 방법)
+// (리플렉션 : 런타임에 클래스,객체의 메타데이터에 접근하여 관련 정보를 동적으로 조회) -> 런타임 중 타입 검사, 속성 접근, 인스턴스 생성 등
+// UCLASS : 클래스 정의, 해당 클래스를 리플렉션에 포함시킴 (UClass를 구현하여 리플렉션의 다양한 정보를 저장, Field 등)
+// USTRUCT : 구조체를 정의
+// UPROPERTY : 멤버 변수를 리플렉션에 포함 (변수 노출 및 편집기에서 접근 가능하도록)
+// 
+// IsA<> : 객체 타입 확인
+// GetProperty() : 속성 접근, 동적으로 읽거나 수정
+// NewObject<> 함수를 통해 동적 객체 생성
+// 
+// TSubclassof : 특정 클래스의 서브클래스를 타입정보
+// 특정 클래스의 서브 클래스를 참조할 수 있는 템플릿 클래스
+// (서브 클래스 : 자식 클래스)
+// (TSubclassof<> 를 통해 특정 클래스만 허용하도록 제한 가능 - 안전한 참조)
+// 
+// CDO(Class Default Object) : 클래스의 기본 인스턴스
+// 클래스가 처음 로드될 때 자동 생성 (해당 클래스의 모든 인스턴스가 공통으로 사용하는 기본값 제공)
+//
+// 언리얼은 각종 매크로들을 이용하여 리플렉션을 구현한 상황이다
+// + 빌드가 오래걸리는 이유 중 하나는 각 클래스들을 Uclass 버전으로 빌드하기에
+template <typename AssetType>
+AssetType* USampleAssetManager::GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepsInMemory)
+{
+	AssetType* LoadedAsset = nullptr;
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		// 로딩이 되어있다? -> 바로 가져옴
+		// 로딩이 안되어 있다 -> Null
+		LoadedAsset = AssetPointer.Get();
+		if (!LoadedAsset)
+		{
+			// 동기 에셋 로드
+			LoadedAsset = Cast<AssetType>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedAsset, TEXT("Failed to load asset [%s]"), *AssetPointer.ToString());
+		}
+
+		// 메모리에 남겨두는지 확인
+		if (LoadedAsset && bKeepsInMemory)
+		{
+			// 여기서 AddLoadAsset은 메모리에 상주하기 위한 장치라고 생각하면 됨:
+			// - 한번 등록되면 직접 내리지 않는한 Unload가 되지 않음 (== 캐싱)
+			Get().AddLoadedAsset(Cast<UObject>(LoadedAsset));
+		}
+	}
+
+	return LoadedAsset;
+}
+
+// 반환을 클래스 타입으로
+template <typename AssetType>
+TSubclassOf<AssetType> USampleAssetManager::GetSubclass(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	// AssetType 에 해당하는 녀석들만 참조로 가지려 함
+	TSubclassOf<AssetType> LoadedSubclass;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+	if (AssetPath.IsValid())
+	{
+		// 적절한 클래스가 매칭되었는지를 확인
+		LoadedSubclass = AssetPointer.Get();
+		if (!LoadedSubclass)
+		{
+			LoadedSubclass = Cast<UClass>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedSubclass, TEXT("Failed to load asset class [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedSubclass && bKeepInMemory)
+		{
+			Get().AddLoadedAsset(Cast<UObject>(LoadedSubclass));
+		}
+	}
+
+	return LoadedSubclass;
+}
