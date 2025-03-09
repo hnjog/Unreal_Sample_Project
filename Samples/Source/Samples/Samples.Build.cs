e50eb682f6b0c233e103e2ea94451c2ed51b0385
Hyun <hnjogo@gmail.com>
2025-03-09 18:05:39 +0900

    신규 플러그인(GAS,GameFeatures,Modular)

--- Samples/Source/Samples/Samples.Build.cs
+++ Samples/Source/Samples/Samples.Build.cs
@@ -8,7 +8,16 @@ public class Samples : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			// GAS
+			"GameplayTags",
+			// Game Features
+			"ModularGameplay",
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
