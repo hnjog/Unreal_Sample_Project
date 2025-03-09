8a782099a688566bf16db79037d77174553009e7
Hyun <hnjogo@gmail.com>
2025-03-09 20:44:59 +0900

    Experience 로드와 관련된 enum 및 Delegate

--- Samples/Source/Samples/GameModes/SampleExperienceManagerComponent.cpp
+++ Samples/Source/Samples/GameModes/SampleExperienceManagerComponent.cpp
@@ -1,2 +1,26 @@
-#include "SampleExperienceManagerComponent.h"
+﻿#include "SampleExperienceManagerComponent.h"
 
+void USampleExperienceManagerComponent::CallOrRegister_OnExperienceLoaded(FOnSampleExperienceLoaded::FDelegate&& Delegate)
+{
+	if (IsExperienceLoaded())
+	{
+		// 요청하는 함수에게 이미 로딩이 되었으므로
+		// 현재 Experience를 인자로 건네주며 실행시킨다(callback)
+		Delegate.Execute(CurrentExperience);
+	}
+	else
+	{
+		// movetemp 를 통해 rvalue Reference 위치를 이동 시킴
+		// Delegate 리스트에 해당 함수를 등록
+		//
+		// Delegate 객체는 내부적으로 필요한 변수를 메모리 할당해놓음
+		// (함수 인자 등, 여러 상태를 저장하는 변수
+		// 
+		// ex)
+		// TArray<int> a = {1,2,3,4}
+		// delegate_type delegate = [a](){return a.num;}
+		// -> a는 delegate_type 내부에 new로 할당이 된다
+		//  -> 복사 비용을 낮추기 위하여 Move를 통하여 전달
+		OnExperienceLoaded.Add(MoveTemp(Delegate));
+	}
+}
--- Samples/Source/Samples/GameModes/SampleExperienceManagerComponent.h
+++ Samples/Source/Samples/GameModes/SampleExperienceManagerComponent.h
@@ -4,11 +4,62 @@
 #include "Components/GameStateComponent.h"
 #include "SampleExperienceManagerComponent.generated.h"
 
+class USampleExperienceDefinition;
+
+enum class ESampleExperienceLoadState
+{
+	Unloaded,
+	Loading,
+	Loaded,
+	Deactivating,
+};
+
+// Unreal Delegate
+// Multicast : 하나의 이벤트에 여러 함수 연결 가능
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnSampleExperienceLoaded, const USampleExperienceDefinition*);
+
+/*
+	USampleExperienceManagerComponent
+	- GameState를 owner로 가지며, Experience의 상태 정보를 가지는 Component
+	- 추가로 Manager의 역할로서 Experience 로딩 상태 업데이트 및 이벤트를 관리
+*/
+
 UCLASS()
 class SAMPLES_API USampleExperienceManagerComponent : public UGameStateComponent
 {
+	// 리플렉션 기능과 관련한 자동 생성 코드
+
 	GENERATED_BODY()
+public:
+	// 로드 완료 + Experience 존재하는지 체크
+	bool IsExperienceLoaded() { return (LoadState == ESampleExperienceLoadState::Loaded) && (CurrentExperience != nullptr); }
 	
+	// FOnSampleExperienceLoaded::FDelegate - 해당 델리게이트에서 요구하는 함수 타입을 인자로 받는다는 뜻
+	// &&(RValue Reference) 
+	// - '이동'하여 객체를 복사하는 대신 해당 자원을 그대로 사용함
+	// - 리터럴, 람다, 함수반환값 등과 같이 임시 생성된 객체를 포함받을 수 있음
+	// &(LValue Reference)
+	// - 이 방식은 메모리 상에 존재하는 객체만 받을 수 있음
+	// 
+	// 아마 RValue인 람다 or std::function 등만 인자로 받을 수 있어 보임
+	//
+	// OnExperienceLoaded 에 바인딩 하거나, Experience 로딩이 완료되었다면 호출
+	void CallOrRegister_OnExperienceLoaded(FOnSampleExperienceLoaded::FDelegate&& Delegate);
+
+public:
+	// 가리키는 객체를 수정할 수 없도록 const 를 걸어준다
+	// 그래도 ptr은 새로운 Experience를 가르킬 수 있음
+	// c++ 의 type* const 방식
+	//
+	// 로딩 요청을 받아야 로딩을 한다
+	UPROPERTY()
+	TObjectPtr<const USampleExperienceDefinition> CurrentExperience;
+
+	// Experience의 로딩 상태를 모니터링
+	ESampleExperienceLoadState LoadState = ESampleExperienceLoadState::Unloaded;
+
+	// Experience 로딩이 완료된 이후, BroadCasting Delegate
+	FOnSampleExperienceLoaded OnExperienceLoaded;
 
 };
 /*
