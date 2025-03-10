dd8a73247863aafc6475c1ad0188ec0863af0723
Hyun <hnjogo@gmail.com>
2025-03-10 11:48:38 +0900

    PlayerState도 ExperienceLoaded 구현 및 등록
    -> PawnData는 나중에 Game Ability System Component를 위한 것
    -> 가져오려면 Experience가 Load 될때 가져와야 하므로

--- Samples/Source/Samples/Player/SamplePlayerState.cpp
+++ Samples/Source/Samples/Player/SamplePlayerState.cpp
@@ -1,2 +1,21 @@
-#include "SamplePlayerState.h"
+﻿#include "SamplePlayerState.h"
+#include"../GameModes/SampleExperienceDefinition.h"
+#include"../Character/SamplePawnData.h"
+#include"../GameModes/SampleExperienceManagerComponent.h"
 
+void ASamplePlayerState::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	const AGameStateBase* GameState = GetWorld()->GetGameState();
+	check(GameState);
+
+	USampleExperienceManagerComponent* ExperienceManagerComponent = GameState->FindComponentByClass<USampleExperienceManagerComponent>();
+	check(ExperienceManagerComponent);
+
+	ExperienceManagerComponent->CallOrRegister_OnExperienceLoaded(FOnSampleExperienceLoaded::FDelegate::CreateUObject(this, &ThisClass::OnExperienceLoaded));
+}
+
+void ASamplePlayerState::OnExperienceLoaded(const USampleExperienceDefinition* CurrentExperience)
+{
+}
--- Samples/Source/Samples/Player/SamplePlayerState.h
+++ Samples/Source/Samples/Player/SamplePlayerState.h
@@ -1,12 +1,28 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
 #include "SamplePlayerState.generated.h"
 
+class USampleExperienceDefinition;
+class USamplePawnData;
+
 UCLASS()
 class SAMPLES_API ASamplePlayerState : public APlayerState
 {
 	GENERATED_BODY()
-	
+public:
+
+	// AActor's interface
+	virtual void PostInitializeComponents() final;
+
+	// member method
+	// 폰 데이터 캐싱을 위하여 Experience 로딩 완료때 가져오기 위함
+	void OnExperienceLoaded(const USampleExperienceDefinition* CurrentExperience);
+
+	// Cacheing 하는 이유? (ExperienceManagerComponent에도 있지 않나?)
+	// 나중에 PlayerState에 GAS 컴포넌트를 붙일 예정
+	// -> GAS가 폰데이터를 참조할 것이기에 미리 캐싱 코드를 만들어 놓음
+	UPROPERTY()
+	TObjectPtr<const USamplePawnData> PawnData;
 };
