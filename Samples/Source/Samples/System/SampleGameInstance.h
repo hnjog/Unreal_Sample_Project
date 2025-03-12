121f8f5c92dc1bccb0e0a8bff1ff4e03ea1c50f9
Hyun <hnjogo@gmail.com>
2025-03-12 09:22:54 +0900

    GameInstance Base class

---
+++ Samples/Source/Samples/System/SampleGameInstance.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SampleGameInstance.generated.h"
+
+UCLASS()
+class SAMPLES_API USampleGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
