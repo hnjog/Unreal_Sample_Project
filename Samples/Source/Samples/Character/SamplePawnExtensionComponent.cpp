47f812f5f8d0ad7fde794d7c4af39e533a60a713
Hyun <hnjogo@gmail.com>
2025-03-15 17:14:59 +0900

    OnRegister에서 Pawn 부착 체크

--- Samples/Source/Samples/Character/SamplePawnExtensionComponent.cpp
+++ Samples/Source/Samples/Character/SamplePawnExtensionComponent.cpp
@@ -1,4 +1,5 @@
 ﻿#include "SamplePawnExtensionComponent.h"
+#include "../SampleLogChannels.h"
 
 USamplePawnExtensionComponent::USamplePawnExtensionComponent(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer)
@@ -11,6 +12,19 @@ USamplePawnExtensionComponent::USamplePawnExtensionComponent(const FObjectInitia
 void USamplePawnExtensionComponent::OnRegister()
 {
 	Super::OnRegister();
+
+	// 올바른 Actor에 등록되었는지 확인
+	// -> 확인 위치가 OnRegister() 인 이유?
+	// OnRegister 단계에서 Component를 부착하기에
+	// Component 인, PawnExtension이 생성되어 부착이 되는 상태
+	// => GetPawn이 실패한다면 '부착'이 실패된 문제 상황이므로 Error 를 발생시켜준다
+	{
+		if (!GetPawn<APawn>())
+		{
+			UE_LOG(LogSample, Error, TEXT("this component has been added to a BP Whose base class is not a Pawn!"));
+			return;
+		}
+	}
 }
 
 void USamplePawnExtensionComponent::BeginPlay()
