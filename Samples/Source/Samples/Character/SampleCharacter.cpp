c079faf01db9162669496b3654d2df987cdf7673
Hyun <hnjogo@gmail.com>
2025-03-15 16:55:39 +0900

    주석 추가
    -> Native C++에서 생성자가 호출된 후,
    BP의 Simple Construction 들이 생성되기에,
    각 컴포넌트들을 종속하지 않은채로
    생성 순서를 조절할 수 있음
    (다만, 생성 위치를 BP에 두어야 하는 점을 상기할 것)

--- Samples/Source/Samples/Character/SampleCharacter.cpp
+++ Samples/Source/Samples/Character/SampleCharacter.cpp
@@ -8,6 +8,13 @@ ASampleCharacter::ASampleCharacter()
 	PrimaryActorTick.bCanEverTick = false;
 
 	// PawnExtensionComponent 생성
+	// native C++ 에서 선언한 것이 '먼저 호출'된다
+	// BP에서 생성한 Simple Construction (BP에서 붙인 것들)이 이후에 호출된다
+	// -> 엔진의 생성 순서를 이용하여
+	// 컴포넌트 생성 순서를 관리할 수 있음
+	// (Pawn은 여기서 생성되지만, BP에 생성되는 컴포넌트들은 이 생성자 이후에 생성이 되기에)
+	// (다만 이후 생성되는 컴포넌트들은 BP에서만 생성해야 하는 점을 상기)
+	// (더 파고드려면 Post Initialize 등을 참고하자)
 	PawnExtComponent = CreateDefaultSubobject<USamplePawnExtensionComponent>(TEXT("PawnExtensionComponent"));
 }
 
