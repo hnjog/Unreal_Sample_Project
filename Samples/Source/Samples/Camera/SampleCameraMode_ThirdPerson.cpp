606d065112edc6ab00f4d3f0a979105769578e73
Hyun <hnjogo@gmail.com>
2025-03-18 20:41:14 +0900

    삼인칭 카메라 모드 클래스 생성자 생성

--- Samples/Source/Samples/Camera/SampleCameraMode_ThirdPerson.cpp
+++ Samples/Source/Samples/Camera/SampleCameraMode_ThirdPerson.cpp
@@ -1,2 +1,7 @@
 #include "SampleCameraMode_ThirdPerson.h"
 
+USampleCameraMode_ThirdPerson::USampleCameraMode_ThirdPerson(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+
+}
--- Samples/Source/Samples/Camera/SampleCameraMode_ThirdPerson.h
+++ Samples/Source/Samples/Camera/SampleCameraMode_ThirdPerson.h
@@ -1,14 +1,15 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "SampleCameraMode.h"
 #include "SampleCameraMode_ThirdPerson.generated.h"
 
-
-
+// 추상 클래스이기에 다른 클래스에서 상속받아야 객체 생성이 가능
 UCLASS(Abstract,Blueprintable)
 class SAMPLES_API USampleCameraMode_ThirdPerson : public USampleCameraMode
 {
 	GENERATED_BODY()
-	
+public:
+	USampleCameraMode_ThirdPerson(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
 };
