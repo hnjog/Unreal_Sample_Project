f2c5524cc856c8992047600205b006410d32804e
Hyun <hnjogo@gmail.com>
2025-03-18 20:15:24 +0900

    3인칭용 CameraModeClass 제작

---
+++ Samples/Source/Samples/Camera/SampleCameraMode_ThirdPerson.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SampleCameraMode.h"
+#include "SampleCameraMode_ThirdPerson.generated.h"
+
+
+
+UCLASS(Abstract,Blueprintable)
+class SAMPLES_API USampleCameraMode_ThirdPerson : public USampleCameraMode
+{
+	GENERATED_BODY()
+	
+};
