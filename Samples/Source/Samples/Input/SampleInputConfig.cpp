df9e1cf5ba8831d2c210391c789c3a4e93c2b9ec
Hyun <hnjogo@gmail.com>
2025-03-25 21:58:56 +0900

    InputConfig 멤버 변수, 함수,(Action - Tag) 매핑용 구조체 작성

--- Samples/Source/Samples/Input/SampleInputConfig.cpp
+++ Samples/Source/Samples/Input/SampleInputConfig.cpp
@@ -1,2 +1,16 @@
 #include "SampleInputConfig.h"
 
+USampleInputConfig::USampleInputConfig(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+const UInputAction* USampleInputConfig::FindNativeInputActionForTag(const FGameplayTag& Tag, bool bLogNotFound) const
+{
+	return nullptr;
+}
+
+const UInputAction* USampleInputConfig::FindAbilityInputActionForTag(const FGameplayTag& Tag, bool bLogNotFound) const
+{
+	return nullptr;
+}
--- Samples/Source/Samples/Input/SampleInputConfig.h
+++ Samples/Source/Samples/Input/SampleInputConfig.h
@@ -1,12 +1,42 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
+#include <GameplayTagContainer.h>
 #include "Engine/DataAsset.h"
 #include "SampleInputConfig.generated.h"
+
+class UInputAction;
+
+// GamePlayTag와 InputAction을 연결하는 래퍼 클래스
+// 결과값(Action)과 Tag를 연결해주는 단위
+USTRUCT(BlueprintType)
+struct FSampleInputAction
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<const UInputAction> InputAction = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (Category = "InputTag"))
+	FGameplayTag InputTag;
+};
 
 UCLASS()
 class SAMPLES_API USampleInputConfig : public UDataAsset
 {
 	GENERATED_BODY()
+public:
+	USampleInputConfig(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 	
+	const UInputAction* FindNativeInputActionForTag(const FGameplayTag& Tag, bool bLogNotFound = true) const;
+	const UInputAction* FindAbilityInputActionForTag(const FGameplayTag& Tag, bool bLogNotFound = true) const;
+
+public:
+	// 잘 안바뀌는 액션들 (ex : 이동, 마우스 움직임 등)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputAction"))
+	TArray<FSampleInputAction> NativeInputActions;
+
+	// 그 외의 액션들
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputAction"))
+	TArray<FSampleInputAction> AbilityInputActions;
 };
