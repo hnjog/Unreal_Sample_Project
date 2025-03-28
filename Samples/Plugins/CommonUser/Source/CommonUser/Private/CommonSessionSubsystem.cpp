7a9ed98e0882d1d4ad44c540c17450f8794c6dc0
Hyun <hnjogo@gmail.com>
2025-03-28 10:46:51 +0900

    GetMapName()

--- Samples/Plugins/CommonUser/Source/CommonUser/Private/CommonSessionSubsystem.cpp
+++ Samples/Plugins/CommonUser/Source/CommonUser/Private/CommonSessionSubsystem.cpp
@@ -1,7 +1,16 @@
-﻿#include "CommonSessionSubsystem.h"
+#include "CommonSessionSubsystem.h"
+#include <Engine/AssetManager.h>
 
 FString UCommonSession_HostSessionRequest::GetMapName() const
 {
+	// AssetManager를 통해 MapID를, FAssetData를 반환
+	// PackageName을 반환하여 Map 경로를 얻을 수 있음
+	FAssetData MapAssetData;
+	if (UAssetManager::Get().GetPrimaryAssetData(MapID, MapAssetData))
+	{
+		return MapAssetData.PackageName.ToString();
+	}
+
 	return FString();
 }
 
