c012dcad9f2a72bd251de451beafdbd924cc2194
Hyun <hnjogo@gmail.com>
2025-03-31 19:04:31 +0900

    SampleExperienceActionSet 클래스 생성
    - Character를 상속한 HeroComponent에서 일일이
    DefalutInputConfigs 를 설정해 주는것은 하드코딩에 가까우며, 다양한 모드를 사용할 때 휴먼 에러가 발생하기 쉬워짐
    - 그렇기에 Context를 세팅하는 클래스를 다루는듯하다

---
+++ Samples/Source/Samples/GameModes/SampleExperienceActionSet.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SampleExperienceActionSet.generated.h"
+
+UCLASS()
+class SAMPLES_API USampleExperienceActionSet : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
