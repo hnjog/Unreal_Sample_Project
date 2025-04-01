97823b4fd25ea676cdc56189c064ef390ab65242
Hyun <hnjogo@gmail.com>
2025-04-01 13:13:17 +0900

    GameFeatureAction_WorldActionBase
    -> 특정 PI(World)에서 해당 GameFeatureAction을 호출하는지 알기 위한 일종의 Wrapper 클래스

---
+++ Samples/Source/Samples/GameFeatures/GameFeatureAction_WorldActionBase.cpp
@@ -0,0 +1,18 @@
+﻿#include"GameFeatureAction_WorldActionBase.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(GameFeatureAction_WorldActionBase)
+
+void UGameFeatureAction_WorldActionBase::OnGameFeatureActivating(FGameFeatureActivatingContext& Context)
+{
+	// 월드 순회
+	for (const FWorldContext& WorldContext : GEngine->GetWorldContexts())
+	{
+		// 앞서 GameFeatureAction을 활성화 하면서 Context에 World를 넣어줬기에
+		// 이에 따라 적용할 대상인지를 판단
+		if (Context.ShouldApplyToWorldContext(WorldContext))
+		{
+			// WorldActionBase의 interface 함수 호출하여
+			// 해당 월드에 Action 적용
+			AddToWorld(WorldContext, Context);
+		}
+	}
+}
---
+++ Samples/Source/Samples/GameFeatures/GameFeatureAction_WorldActionBase.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "Containers/Map.h"
+#include "GameFeatureAction.h"
+#include "GameFeaturesSubsystem.h"
+#include "GameFeatureAction_WorldActionBase.generated.h"
+
+class FDelegateHandle;
+class UGameInstance;
+struct FGameFeatureActivatingContext;
+struct FGameFeatureDeactivatingContext;
+struct FWorldContext;
+
+// 어떠한 PI (World)가 호출하는지 모르기에
+// context로 구분하는 용도의 래퍼 클래스
+UCLASS(Abstract)
+class UGameFeatureAction_WorldActionBase : public UGameFeatureAction
+{
+	GENERATED_BODY()
+public:
+	// GameFeatureAction's interface
+	virtual void OnGameFeatureActivating(FGameFeatureActivatingContext& Context) override;
+
+	// interface
+	// 
+	// PURE_VIRTUAL : C++의 Interface 기능이 강하게 없기에
+	// 구현된 Unreal용 매크로 (상속 받는 클래스는 이 함수를 반드시 구현해야 함)
+	//  - 비슷한 C++ 의 순수 가상 함수와 비교하자면?
+	//  ex ) virtual void a() = 0;
+	//  공통점 : 직접 인스턴스 생성 x, 반드시 상속받는 클래스에서 구현해야 함
+	//  차이점 : PURE_VIRTUAL는 내부적으로 빈 구현을 생성하고, 디버깅 정보를 제공
+	//  (C++의 경우는 호출될 시, 런타임 오류 발생)
+	//  (PURE_VIRTUAL는 호출될 시 메시지나 로그를 출력)
+	virtual void AddToWorld(const FWorldContext& WorldContext, const FGameFeatureStateChangeContext& ChangeContext) PURE_VIRTUAL(UGameFeatureAction_WorldActionBase::AddToWorld, );
+};
