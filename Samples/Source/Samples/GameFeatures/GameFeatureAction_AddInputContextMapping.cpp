6612705fb7cc02ef35d333d19f9d278688dffb57
Hyun <hnjogo@gmail.com>
2025-04-05 18:25:43 +0900

    InputMappingContext 를 APlayerController가 추가되면 매핑시켜주는 구현
    -> UGameFrameworkComponentManager의
    AddExtensionHandler를 통해
    등록한 StaticClass의 객체가 생성된 경우,
    등록된 Delegate를 실행
    -> 그렇기에 PlayerController에
     InputMappingContext를 매핑해줄 수 있음

--- Samples/Source/Samples/GameFeatures/GameFeatureAction_AddInputContextMapping.cpp
+++ Samples/Source/Samples/GameFeatures/GameFeatureAction_AddInputContextMapping.cpp
@@ -1,13 +1,148 @@
 ﻿#include "GameFeatureAction_AddInputContextMapping.h"
 #include "InputMappingContext.h"
+#include"Samples/System/SampleAssetManager.h"
+#include "EnhancedInputSubsystems.h"
+#include "UserSettings/EnhancedInputUserSettings.h"
+#include"GameFeatures/Public/GameFeaturesSubsystem.h"
+#include "Components/GameFrameworkComponentManager.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(GameFeatureAction_AddInputContextMapping)
 
 void UGameFeatureAction_AddInputContextMapping::OnGameFeatureActivating(FGameFeatureActivatingContext& Context)
 {
+	FPerContextData& ActiveData = ContextData.FindOrAdd(Context);
+	// 혹시 모르니 다시한번 Reset
+	// 어차피 새로 Level 켜지는 상태
+	if (!ensure(ActiveData.ExtensionRequestHandles.IsEmpty()) ||
+		!ensure(ActiveData.ControllersAddedTo.IsEmpty()))
+	{
+		Reset(ActiveData);
+	}
+
+	// UGameFeatureAction_WorldActionBase를 호출하면서, AddToWorld() 호출!
 	Super::OnGameFeatureActivating(Context);
+}
+
+void UGameFeatureAction_AddInputContextMapping::OnGameFeatureDeactivating(FGameFeatureDeactivatingContext& Context)
+{
+	Super::OnGameFeatureDeactivating(Context);
+	FPerContextData* ActiveData = ContextData.Find(Context);
+
+	// FPerContextData 내부에서
+	// World 와 관련된 데이터들이 이제 필요 없으니
+	// 리셋 시키는 용도
+	if (ensure(ActiveData))
+	{
+		Reset(*ActiveData);
+	}
 }
 
 void UGameFeatureAction_AddInputContextMapping::AddToWorld(const FWorldContext& WorldContext, const FGameFeatureStateChangeContext& ChangeContext)
 {
+	UWorld* World = WorldContext.World();
+	UGameInstance* GameInstance = WorldContext.OwningGameInstance;
+	FPerContextData& ActiveData = ContextData.FindOrAdd(ChangeContext);
+
+	if ((GameInstance != nullptr) && (World != nullptr) && World->IsGameWorld())
+	{
+		// UGameFrameworkComponentManager를 이용하여
+		// ExtensionHandler를 추가하여 등록 
+		// - HandleControllerExtension 를 연결
+		if (UGameFrameworkComponentManager* ComponentManager = UGameInstance::GetSubsystem<UGameFrameworkComponentManager>(GameInstance))
+		{
+			UGameFrameworkComponentManager::FExtensionHandlerDelegate AddAbilitiesDelegate =
+				UGameFrameworkComponentManager::FExtensionHandlerDelegate::CreateUObject(this, &ThisClass::HandleControllerExtension, ChangeContext);
+
+			// 등록된 콜백 함수의 Handle을 ActiveData의 ExtensionRequestHandles 에 등록
+			// AddExtensionHandler(클래스,핸들러(델리게이트) )
+			//  : 해당하는 클래스의 객체가 생성될때, 등록된 델리게이트 호출
+			// 
+			// - APlayerController 가 새로 생성되는 경우,
+			// AddAbilitiesDelegate 가 broadCast하여 등록한 함수를 실행
+			// 
+			// 새로운 플레이어가 들어오면 이런식으로 새로운 InputMappingContext를 등록한다
+			//
+			TSharedPtr<FComponentRequestHandle> ExtensionRequestHandle =
+				ComponentManager->AddExtensionHandler(APlayerController::StaticClass(), AddAbilitiesDelegate);
+
+			ActiveData.ExtensionRequestHandles.Add(ExtensionRequestHandle);
+		}
+	}
+}
+
+void UGameFeatureAction_AddInputContextMapping::Reset(FPerContextData& ActiveData)
+{
+	// ExtensionRequestHandles을 초기화
+	ActiveData.ExtensionRequestHandles.Empty();
+
+	// PawnsAddedTo에 대해서, 하나씩 Stack 방식으로 위에서 아래로 직접 APlayerController를 제거 진행
+	while (!ActiveData.ControllersAddedTo.IsEmpty())
+	{
+		TWeakObjectPtr<APlayerController> ControllerPtr = ActiveData.ControllersAddedTo.Top();
+		if (ControllerPtr.IsValid())
+		{
+			RemoveInputMapping(ControllerPtr.Get(), ActiveData);
+		}
+		else
+		{
+			// WeakObjectPtr로 PawnsAddedTo를 관리하고 있기 때문에, GC되었다면, nullptr일 수 있음
+			ActiveData.ControllersAddedTo.Pop();
+		}
+	}
+}
+
+void UGameFeatureAction_AddInputContextMapping::AddInputMappingForPlayer(UPlayer* Player, FPerContextData& ActiveData)
+{
+	if (ULocalPlayer* LocalPlayer = Cast<ULocalPlayer>(Player))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* InputSystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			for (const FInputMappingContextAndPriority& Entry : InputMappings)
+			{
+				if (const UInputMappingContext* IMC = Entry.InputMapping.Get())
+				{
+					InputSystem->AddMappingContext(IMC, Entry.Priority);
+				}
+			}
+		}
+	}
+
+	// Unreal 에선 이 코드가 없는데 왜 그럴까...?
+	// 딱히 lyra에서 직접적으로 ControllersAddedTo 에 값을 넣는 것처럼 보이진 않음
+	if(APlayerController* PC = Player->PlayerController)
+		ActiveData.ControllersAddedTo.AddUnique(PC);
+}
+
+void UGameFeatureAction_AddInputContextMapping::RemoveInputMapping(APlayerController* PlayerController, FPerContextData& ActiveData)
+{
+	if (ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* InputSystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			for (const FInputMappingContextAndPriority& Entry : InputMappings)
+			{
+				if (const UInputMappingContext* IMC = Entry.InputMapping.Get())
+				{
+					InputSystem->RemoveMappingContext(IMC);
+				}
+			}
+		}
+	}
+
+	ActiveData.ControllersAddedTo.Remove(PlayerController);
+}
+
+void UGameFeatureAction_AddInputContextMapping::HandleControllerExtension(AActor* Actor, FName EventName, FGameFeatureStateChangeContext ChangeContext)
+{
+	APlayerController* AsController = CastChecked<APlayerController>(Actor);
+	FPerContextData& ActiveData = ContextData.FindOrAdd(ChangeContext);
+
+	if ((EventName == UGameFrameworkComponentManager::NAME_ExtensionRemoved))
+	{
+		RemoveInputMapping(AsController, ActiveData);
+	}
+	else if ((EventName == UGameFrameworkComponentManager::NAME_ExtensionAdded))
+	{
+		AddInputMappingForPlayer(AsController->GetLocalPlayer(), ActiveData);
+	}
 }
--- Samples/Source/Samples/GameFeatures/GameFeatureAction_AddInputContextMapping.h
+++ Samples/Source/Samples/GameFeatures/GameFeatureAction_AddInputContextMapping.h
@@ -5,6 +5,7 @@
 #include "GameFeatureAction_AddInputContextMapping.generated.h"
 
 class UInputMappingContext;
+struct FComponentRequestHandle;
 
 USTRUCT()
 struct FInputMappingContextAndPriority
@@ -28,9 +29,52 @@ class UGameFeatureAction_AddInputContextMapping final : public UGameFeatureActio
 {
 	GENERATED_BODY()
 
+	// UGameFeatureAction Interface
 public:
+	// 기본적으로 특정한 게임 기능이 활성화 되거나 비활성화 될때 호출되는 함수들
+	// ex : 레벨 로드, 게임 모드 변경, 게임 세션 시작, 플러그인 활성화 
 	virtual void OnGameFeatureActivating(FGameFeatureActivatingContext& Context) override;
+	// ex : 레벨 언로드, 게임 모드 변경, 게임 세션 종료, 플러그인 비활성화 
+	virtual void OnGameFeatureDeactivating(FGameFeatureDeactivatingContext& Context) override;
 
+	// UGameFeatureAction_WorldActionBase Interface
 public:
 	virtual void AddToWorld(const FWorldContext& WorldContext, const FGameFeatureStateChangeContext& ChangeContext) override;
+
+	// Member Variable
+public:
+	// 각각의 컨텍스트에 대한 관련 데이터
+	// GameFeature가 엔진 쪽의 개념이기에
+	// 적용할 World에 대한 데이터를 가져다 캐싱해 놓는 느낌
+	// 일일이 전부 World 순회하지 않기 위해
+	struct FPerContextData
+	{
+		TArray<TSharedPtr<FComponentRequestHandle>> ExtensionRequestHandles;
+		TArray<TWeakObjectPtr<APlayerController>> ControllersAddedTo;
+	};
+
+	// UInputMappingContext 을 WeakPtr로 들고 있는 구조체
+	// 그것을 array로 가지고 있는다
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TArray<FInputMappingContextAndPriority> InputMappings;
+
+	TMap<FGameFeatureStateChangeContext, FPerContextData> ContextData;
+	FDelegateHandle RegisterInputContextMappingsForGameInstanceHandle;
+
+	// Member Method
+public:
+	// ActiveData 를 초기화
+	// Engine Subsystem은 PI가 사라져도 리셋되지 않기에
+	// 매핑을 끊기 위한 함수
+	void Reset(FPerContextData& ActiveData);
+
+	// GameFeatureState가 변경될 때의 콜백 함수
+	// (or ExtensionEvent를 GameFrameworkComponentManager를 통해 호출 가능)
+	void HandleControllerExtension(AActor* Actor, FName EventName, FGameFeatureStateChangeContext ChangeContext);
+
+	// UEnhancedInputLocalPlayerSubsystem 에
+	// MappingContext를 추가
+	void AddInputMappingForPlayer(UPlayer* Player, FPerContextData& ActiveData);
+	void RemoveInputMapping(APlayerController* PlayerController, FPerContextData& ActiveData);
+
 };
