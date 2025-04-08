7a3889801cd124ea24745346ad8763696819212c
Hyun <hnjogo@gmail.com>
2025-04-08 11:56:57 +0900

    사거리가 있는 WeaponInstance 클래스 생성

---
+++ Samples/Source/Samples/Weapons/SampleRangedWeaponInstance.cpp
@@ -0,0 +1,2 @@
+#include "SampleRangedWeaponInstance.h"
+
---
+++ Samples/Source/Samples/Weapons/SampleRangedWeaponInstance.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SampleWeaponInstance.h"
+#include "SampleRangedWeaponInstance.generated.h"
+
+UCLASS()
+class SAMPLES_API USampleRangedWeaponInstance : public USampleWeaponInstance
+{
+	GENERATED_BODY()
+	
+};
