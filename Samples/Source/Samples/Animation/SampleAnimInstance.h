db4efda001fdf9396322543693dca437f035c8cc
Hyun <hnjogo@gmail.com>
2025-04-08 10:08:14 +0900

    AnimInstance 생성

---
+++ Samples/Source/Samples/Animation/SampleAnimInstance.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SampleAnimInstance.generated.h"
+
+UCLASS()
+class SAMPLES_API USampleAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	// 해당 속성은 Lyra의 AnimBP에서 사용되는 값이므로 정의 필요
+	UPROPERTY(BlueprintReadOnly, Category = "Character State Data")
+	float GroundDistance = -1.0f;
+};
