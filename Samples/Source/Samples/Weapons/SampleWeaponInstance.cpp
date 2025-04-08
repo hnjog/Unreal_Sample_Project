314ea293b56ff18220dc372e302e6345b9d7c59f
Hyun <hnjogo@gmail.com>
2025-04-08 11:47:27 +0900

    WeaponInstance 생성

---
+++ Samples/Source/Samples/Weapons/SampleWeaponInstance.cpp
@@ -0,0 +1,2 @@
+#include "SampleWeaponInstance.h"
+
---
+++ Samples/Source/Samples/Weapons/SampleWeaponInstance.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Samples/Equipment/SampleEquipmentInstance.h"
+#include "SampleWeaponInstance.generated.h"
+
+UCLASS()
+class SAMPLES_API USampleWeaponInstance : public USampleEquipmentInstance
+{
+	GENERATED_BODY()
+	
+};
