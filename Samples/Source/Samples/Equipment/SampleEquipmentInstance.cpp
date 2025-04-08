c4a5188be95c79e92e722a37455154dc2e554de7
Hyun <hnjogo@gmail.com>
2025-04-08 11:47:19 +0900

    EquipmentInstance 생성
    -> Equip 할 클래스의 부모 클래스

---
+++ Samples/Source/Samples/Equipment/SampleEquipmentInstance.cpp
@@ -0,0 +1,2 @@
+#include "SampleEquipmentInstance.h"
+
---
+++ Samples/Source/Samples/Equipment/SampleEquipmentInstance.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SampleEquipmentInstance.generated.h"
+
+// 장착이 가능한 'Equipment'들이 상속받을 클래스
+UCLASS()
+class SAMPLES_API USampleEquipmentInstance : public UObject
+{
+	GENERATED_BODY()
+	
+};
