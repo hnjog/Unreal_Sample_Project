484c988c6201a5eae0f7a50ad06e6abfc2ac40d2
Hyun <hnjogo@gmail.com>
2025-04-08 18:05:50 +0900

    USampleInventoryManagerComponent 정의
    -> Cosmetic과 비슷한 구조
    List 구조체 -> Entry 구조체 -> 실제 관리할 데이터

--- Samples/Source/Samples/Inventory/SampleInventoryManagerComponent.cpp
+++ Samples/Source/Samples/Inventory/SampleInventoryManagerComponent.cpp
@@ -1,31 +1,9 @@
-#include "SampleInventoryManagerComponent.h"
+﻿#include "SampleInventoryManagerComponent.h"
+#include "SampleInventoryItemInstance.h"
 
-// Sets default values for this component's properties
-USampleInventoryManagerComponent::USampleInventoryManagerComponent()
+USampleInventoryManagerComponent::USampleInventoryManagerComponent(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer),
+	InventoryList(this) // ActorComponent 인 자신을 전달
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
-
-
-// Called when the game starts
-void USampleInventoryManagerComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-
-// Called every frame
-void USampleInventoryManagerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
--- Samples/Source/Samples/Inventory/SampleInventoryManagerComponent.h
+++ Samples/Source/Samples/Inventory/SampleInventoryManagerComponent.h
@@ -1,26 +1,56 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "SampleInventoryManagerComponent.generated.h"
 
+class USampleInventoryItemInstance;
 
+USTRUCT(BlueprintType)
+struct FSampleInventoryEntry
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY()
+	TObjectPtr<USampleInventoryItemInstance> Instance = nullptr;
+
+};
+
+// Inventory Item 관리 객체
+USTRUCT(BlueprintType)
+struct FSampleInventoryList
+{
+	GENERATED_BODY()
+public:
+	FSampleInventoryList(UActorComponent* InOwnerComponent = nullptr)
+		:OwnerComponent(InOwnerComponent)
+	{
+
+	}
+
+public:
+	UPROPERTY()
+	TArray<FSampleInventoryEntry> Entries;
+
+	UPROPERTY()
+	TObjectPtr<UActorComponent> OwnerComponent;
+};
+
+// PlayerController의 Component로서 Inventory 관리
+// - 사실 UActorComponent 상속이 아닌 UControllerComponent를 상속받아도? (lyra에선 ActorComponent)
+// (UControllerComponent 를 통해 PlayerController를 가져오기 쉬우므로)
+// 
+// Cosmetic과 유사
+// (Dedicate 서버 기반이라서 Network Serilized Array 시스템을 사용하는듯)
+// 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SAMPLES_API USampleInventoryManagerComponent : public UActorComponent
 {
 	GENERATED_BODY()
+public:
+	USampleInventoryManagerComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
-public:	
-	// Sets default values for this component's properties
-	USampleInventoryManagerComponent();
-
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
+public:
+	UPROPERTY()
+	FSampleInventoryList InventoryList;
 };
