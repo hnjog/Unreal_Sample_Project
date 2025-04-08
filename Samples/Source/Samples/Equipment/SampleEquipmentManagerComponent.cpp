f6dd0bcaa78d1bf55579766b9fb4949f35b623f5
Hyun <hnjogo@gmail.com>
2025-04-08 20:17:49 +0900

    USampleEquipmentManagerComponent 구현
    -> Cosmetic에서 본 그 패턴
    (Dedicate 서버 를 고려한 것)

--- Samples/Source/Samples/Equipment/SampleEquipmentManagerComponent.cpp
+++ Samples/Source/Samples/Equipment/SampleEquipmentManagerComponent.cpp
@@ -1,2 +1,9 @@
 #include "SampleEquipmentManagerComponent.h"
+#include"SampleEquipmentDefinition.h"
+#include"SampleEquipmentInstance.h"
 
+USampleEquipmentManagerComponent::USampleEquipmentManagerComponent(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer),
+	EquipmentList(this) // UPawnC -> UGameFramewC -> UAC
+{
+}
--- Samples/Source/Samples/Equipment/SampleEquipmentManagerComponent.h
+++ Samples/Source/Samples/Equipment/SampleEquipmentManagerComponent.h
@@ -1,12 +1,58 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Components/PawnComponent.h"
 #include "SampleEquipmentManagerComponent.generated.h"
 
+class USampleEquipmentDefinition;
+class USampleEquipmentInstance;
+
+USTRUCT(BlueprintType)
+struct FSampleAppliedEquipmentEntry
+{
+	GENERATED_BODY()
+public:
+	// 장착물에 대한 메타 데이터
+	UPROPERTY()
+	TSubclassOf<USampleEquipmentDefinition> EquipmentDefinition;
+
+	// EquipmentDefinition을 통해 생성된 인스턴스
+	UPROPERTY()
+	TObjectPtr<USampleEquipmentInstance> Instance = nullptr;
+
+};
+
+// EquimentInstance의 인스턴스는 Entry에서 관리
+// - FSampleEquipmentList 는 생성된 객체를 관리
+USTRUCT(BlueprintType)
+struct FSampleEquipmentList
+{
+	GENERATED_BODY()
+public:
+	FSampleEquipmentList(UActorComponent* InOwnerComponent = nullptr)
+		:OwnerComponent(InOwnerComponent)
+	{
+
+	}
+
+public:
+
+	UPROPERTY()
+	TArray<FSampleAppliedEquipmentEntry> Entries;
+
+	UPROPERTY()
+	TObjectPtr<UActorComponent> OwnerComponent;
+};
+
+// Pawn Component 로서 장착물에 대한 관리 
 UCLASS()
 class SAMPLES_API USampleEquipmentManagerComponent : public UPawnComponent
 {
 	GENERATED_BODY()
-	
+public:
+	USampleEquipmentManagerComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+public:
+	UPROPERTY()
+	FSampleEquipmentList EquipmentList;
 };
