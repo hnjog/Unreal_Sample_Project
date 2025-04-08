515b7e0751d864cf5aeaece7e3afd845ee46d976
Hyun <hnjogo@gmail.com>
2025-04-08 14:16:33 +0900

    Equipment 클래스들에 대한 BluePrint 적용 수정

--- Samples/Source/Samples/Equipment/SampleEquipmentInstance.h
+++ Samples/Source/Samples/Equipment/SampleEquipmentInstance.h
@@ -5,7 +5,7 @@
 #include "SampleEquipmentInstance.generated.h"
 
 // 장착이 가능한 'Equipment'들이 상속받을 클래스
-UCLASS()
+UCLASS(BlueprintType,Blueprintable)
 class SAMPLES_API USampleEquipmentInstance : public UObject
 {
 	GENERATED_BODY()
