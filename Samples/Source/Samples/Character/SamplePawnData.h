8148a36d30e45213a9daa3beb1d06344e616d889
Hyun <hnjogo@gmail.com>
2025-04-12 12:07:33 +0900

    pawnData 주석

--- Samples/Source/Samples/Character/SamplePawnData.h
+++ Samples/Source/Samples/Character/SamplePawnData.h
@@ -28,6 +28,10 @@ public:
 	TObjectPtr<USampleInputConfig> InputConfig;
 
 	// 해당하는 Pawn의 AbilitySystem에서 허용할 AbilitySets
+	// -> 이 데이터는 '공용 폰' 이기에 
+	// 모든 폰에서 공용으로 사용하는 '스킬' 이나 '행동'을 넣어둔다
+	// (ex : 점프, 벽타기 등)
+	//
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sample|Abilities")
 	TArray<TObjectPtr<USampleAbilitySet>> AbilitySets;
 };
