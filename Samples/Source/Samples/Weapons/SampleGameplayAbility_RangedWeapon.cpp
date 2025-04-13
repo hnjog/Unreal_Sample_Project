d861634484d2ac0554c650d41213a13a5bfc5833
Hyun <hnjogo@gmail.com>
2025-04-13 09:20:38 +0900

    무기용(pistol) Ability Class 생성

---
+++ Samples/Source/Samples/Weapons/SampleGameplayAbility_RangedWeapon.cpp
@@ -0,0 +1,6 @@
+﻿#include "SampleGameplayAbility_RangedWeapon.h"
+
+USampleGameplayAbility_RangedWeapon::USampleGameplayAbility_RangedWeapon(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
---
+++ Samples/Source/Samples/Weapons/SampleGameplayAbility_RangedWeapon.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Samples/Equipment/SampleGameplayAbility_FromEquipment.h"
+#include "SampleGameplayAbility_RangedWeapon.generated.h"
+
+UCLASS()
+class SAMPLES_API USampleGameplayAbility_RangedWeapon : public USampleGameplayAbility_FromEquipment
+{
+	GENERATED_BODY()
+public:
+	USampleGameplayAbility_RangedWeapon(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+};
