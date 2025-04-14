9867b725ce7fa5fb1167f99543e77fc3ec3b3e99
Hyun <hnjogo@gmail.com>
2025-04-14 12:15:10 +0900

    무기의 사거리, 범위 옵션

--- Samples/Source/Samples/Weapons/SampleRangedWeaponInstance.h
+++ Samples/Source/Samples/Weapons/SampleRangedWeaponInstance.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "SampleWeaponInstance.h"
@@ -8,5 +8,13 @@ UCLASS()
 class SAMPLES_API USampleRangedWeaponInstance : public USampleWeaponInstance
 {
 	GENERATED_BODY()
-	
+public:
+
+	// 유효 사거리
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "WeaponConfig", meta = (ForceUnits = cm))
+	float MaxDamageRange = 25000.0f;
+
+	// 총탄 범위
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "WeaponConfig", meta = (ForceUnits = cm))
+	float BulletTraceWeaponRadius = 0.0f;
 };
