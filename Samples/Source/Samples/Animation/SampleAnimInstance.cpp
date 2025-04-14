c832edd296a1fc4181ff177882ca95b1da3d780d
Hyun <hnjogo@gmail.com>
2025-04-14 17:10:06 +0900

    USampleAnimInstance - FGameplayTagBlueprintPropertyMap 을 통한 매핑
    -> AnimInstance의 boolean값과
     GameplayTag를 서로 매칭시켜주는 구조체

--- Samples/Source/Samples/Animation/SampleAnimInstance.cpp
+++ Samples/Source/Samples/Animation/SampleAnimInstance.cpp
@@ -1,2 +1,23 @@
-#include "SampleAnimInstance.h"
+﻿#include "SampleAnimInstance.h"
+#include "AbilitySystemGlobals.h"
 
+void USampleAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (AActor* OwningActor = GetOwningActor())
+	{
+		if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwningActor))
+		{
+			InitializeWithAbilitySystem(ASC);
+		}
+
+	}
+}
+
+void USampleAnimInstance::InitializeWithAbilitySystem(UAbilitySystemComponent* ASC)
+{
+	// ASC 내부의 GameplayTag와 AnimInstance의 멤버 Property와 Delegate를 연결하여
+	// 값 변화에 대한 반영 진행
+	GameplayTagPropertyMap.Initialize(this, ASC);
+}
--- Samples/Source/Samples/Animation/SampleAnimInstance.h
+++ Samples/Source/Samples/Animation/SampleAnimInstance.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
 #include "Animation/AnimInstance.h"
 #include "SampleAnimInstance.generated.h"
 
@@ -19,7 +20,23 @@ class SAMPLES_API USampleAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 public:
+	// Animation이 최초로 초기화 될때 호출
+	virtual void NativeInitializeAnimation() override;
+
+	void InitializeWithAbilitySystem(UAbilitySystemComponent* ASC);
+
+public:
 	// 해당 속성은 Lyra의 AnimBP에서 사용되는 값이므로 정의 필요
 	UPROPERTY(BlueprintReadOnly, Category = "Character State Data")
 	float GroundDistance = -1.0f;
+
+	// GameplayTag와 AnimInstance의 속성값을 매핑
+	// 
+	// FGameplayTagBlueprintPropertyMap
+	// - AnimInstacne의 boolean 값과
+	//   GameplayTag를 서로 매칭시켜준다
+	//   (Tag 부여로 해당 매핑된 boolean 값을 true,
+	//    제거로 false로 만들 수 있음)
+	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
 };
