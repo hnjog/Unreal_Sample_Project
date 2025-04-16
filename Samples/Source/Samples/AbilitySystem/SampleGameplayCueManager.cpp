31e8ea6f70060238221c66d34312289c61263b88
Hyun <hnjogo@gmail.com>
2025-04-16 09:01:35 +0900

    USampleGameplayCueManager
    -> Asset Manager에 필요 에셋을 올리는 역할을 포함하는 래핑용 클래스
    -> Effect 와 관련된 렌더링 정보이기에
        Server에선 해당 데이터를 AssetManaer를 통해
        메모리에 굳이 로드할 필요가 없음

---
+++ Samples/Source/Samples/AbilitySystem/SampleGameplayCueManager.cpp
@@ -0,0 +1,42 @@
+﻿#include "SampleGameplayCueManager.h"
+#include "AbilitySystemGlobals.h"
+#include "GameplayCueSet.h"
+#include "Engine/AssetManager.h"
+
+USampleGameplayCueManager::USampleGameplayCueManager(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+USampleGameplayCueManager* USampleGameplayCueManager::Get()
+{
+	// 유일성 보장
+	return Cast<USampleGameplayCueManager>(UAbilitySystemGlobals::Get().GetGameplayCueManager());
+}
+
+const FPrimaryAssetType UFortAssetManager_GameplayCueRefsType = TEXT("GameplayCueRefs");
+const FName UFortAssetManager_GameplayCueRefsName = TEXT("GameplayCueReferences");
+const FName UFortAssetManager_LoadStateClient = FName(TEXT("Client"));
+
+void USampleGameplayCueManager::RefreshGameplayCuePrimaryAsset()
+{
+	TArray<FSoftObjectPath> CuePaths;
+	UGameplayCueSet* RuntimeGameplayCueSet = GetRuntimeCueSet();
+	if (RuntimeGameplayCueSet)
+	{
+		RuntimeGameplayCueSet->GetSoftObjectPaths(CuePaths);
+	}
+
+	// 새로 추가된 에셋의 경로를 CuePaths에 추가하여!
+	// UFortAssetManager_LoadStateClient : 클라이언트
+	// 클라이언트라면 에셋매니저에 올려달라 하지만, 서버에선 올리지 않음
+	FAssetBundleData BundleData;
+	BundleData.AddBundleAssetsTruncated(UFortAssetManager_LoadStateClient, CuePaths);
+
+	// PrimaryAssetId를 고정된 이름으로 설정하여 (const로 설정되어 있음)
+	FPrimaryAssetId PrimaryAssetId = FPrimaryAssetId(UFortAssetManager_GameplayCueRefsType, UFortAssetManager_GameplayCueRefsName);
+
+	// 에셋 매니저에 추가 진행
+	UAssetManager::Get().AddDynamicAsset(PrimaryAssetId, FSoftObjectPath(), BundleData);
+
+}
---
+++ Samples/Source/Samples/AbilitySystem/SampleGameplayCueManager.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueManager.h"
+#include "SampleGameplayCueManager.generated.h"
+
+/*
+	Gameplay Cue는 Effect를 
+	보다 쉽게, '반응형 트리거'로서 이용하게 만드는
+	일종의 렌더링 관련 정보
+
+	그말은?
+	-> 서버에선 굳이 메모리에 로드할 필요 없음
+*/
+UCLASS()
+class SAMPLES_API USampleGameplayCueManager : public UGameplayCueManager
+{
+	GENERATED_BODY()
+public:
+	USampleGameplayCueManager(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+public:
+	static USampleGameplayCueManager* Get();
+
+	void RefreshGameplayCuePrimaryAsset();
+
+};
