43af57a0d11da7281c453384a338dbb123c8643a
Hyun <hnjogo@gmail.com>
2025-04-18 15:43:21 +0900

    신규 plugin CommonGame 생성
    -> CommonUI의 Wrapping 용 Plugin

---
+++ Samples/Plugins/CommonGame/Source/CommonGame/CommonGame.Build.cs
@@ -0,0 +1,37 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CommonGame : ModuleRules
+{
+	public CommonGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+                "CoreUObject",
+                "InputCore",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "UMG",
+                "CommonUI",
+                "CommonUser",
+                "GameplayTags",
+                "ModularGameplayActors",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+			}
+			);
+		
+		
+	}
+}
