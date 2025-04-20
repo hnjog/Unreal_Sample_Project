7ee07a4f4be490342216b242dc67beb6e7cf3917
Hyun <hnjogo@gmail.com>
2025-04-20 17:46:10 +0900

    layout 주석

--- Samples/Plugins/CommonGame/Source/CommonGame/Public/PrimaryGameLayout.h
+++ Samples/Plugins/CommonGame/Source/CommonGame/Public/PrimaryGameLayout.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "CommonUserWidget.h"
@@ -10,3 +10,10 @@ class COMMONGAME_API UPrimaryGameLayout : public UCommonUserWidget
 	GENERATED_BODY()
 	
 };
+
+/*
+	가장 큰 레이아웃(하나만 존재)
+
+	이 내부에 다양한 UI를 세팅하기에,
+	제일 먼저 등록되어야 한다
+*/
