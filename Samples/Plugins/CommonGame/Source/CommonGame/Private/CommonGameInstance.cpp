50391aa614b2106bfee6e877d7f032297d17bf7f
Hyun <hnjogo@gmail.com>
2025-04-20 17:50:21 +0900

    localplayer 입장시, UI 설정하도록 GameInstance에서 설정

--- Samples/Plugins/CommonGame/Source/CommonGame/Private/CommonGameInstance.cpp
+++ Samples/Plugins/CommonGame/Source/CommonGame/Private/CommonGameInstance.cpp
@@ -1,2 +1,34 @@
-#include "CommonGameInstance.h"
+﻿#include "CommonGameInstance.h"
+#include "GameUIManagerSubsystem.h"
+#include "CommonLocalPlayer.h"
 
+int32 UCommonGameInstance::AddLocalPlayer(ULocalPlayer* NewPlayer, FPlatformUserId UserId)
+{
+    // GameInstance에 관리하는 Player 컨테이너에 추가
+    // (ReturnVal 은 새로 추가된 Index)
+    int32 ReturnVal = Super::AddLocalPlayer(NewPlayer, UserId);
+    
+    if (ReturnVal != INDEX_NONE)
+    {
+        // PrimaryPlayer는 처음만 캐싱 (아마도 Index == 0인 맨 처음 player를 지정하는듯)
+        if (!PrimaryPlayer.IsValid())
+        {
+            PrimaryPlayer = NewPlayer;
+        }
+
+        // GameUIManagerSubsystem 을 통해
+        // NotifyPlayerAdded 호출로 GameLayout을 추가
+        GetSubsystem<UGameUIManagerSubsystem>()->NotifyPlayerAdded(Cast<UCommonLocalPlayer>(NewPlayer));
+    }
+
+    return ReturnVal;
+}
+
+bool UCommonGameInstance::RemoveLocalPlayer(ULocalPlayer* ExistingPlayer)
+{
+    if (PrimaryPlayer == ExistingPlayer)
+        PrimaryPlayer.Reset();
+
+    GetSubsystem<UGameUIManagerSubsystem>()->NotifyPlayerDestroyed(Cast<UCommonLocalPlayer>(ExistingPlayer));
+    return Super::RemoveLocalPlayer(ExistingPlayer);
+}
--- Samples/Plugins/CommonGame/Source/CommonGame/Public/CommonGameInstance.h
+++ Samples/Plugins/CommonGame/Source/CommonGame/Public/CommonGameInstance.h
@@ -1,12 +1,23 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "CommonGameInstance.generated.h"
 
+// LocalPlayer가 PlayerController에 등록되는 것을 확인하고
+// PrimaryGameLayout을 등록
+
 UCLASS(Abstract)
 class COMMONGAME_API UCommonGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+public:
+	// UGameInstance Interface
+	virtual int32 AddLocalPlayer(ULocalPlayer* NewPlayer, FPlatformUserId UserId) override;
+	virtual bool RemoveLocalPlayer(ULocalPlayer* ExistingPlayer) override;
+
+public:
+	// 메인 로컬 플레이어 캐싱
+	TWeakObjectPtr<ULocalPlayer> PrimaryPlayer;
+
 };
