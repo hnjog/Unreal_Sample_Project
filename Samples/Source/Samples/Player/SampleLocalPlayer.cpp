16fd5851571e5bdedafd179daf1d933a2a5750d3
Hyun <hnjogo@gmail.com>
2025-04-20 17:58:14 +0900

    래핑용 local player class 추가

---
+++ Samples/Source/Samples/Player/SampleLocalPlayer.cpp
@@ -0,0 +1,2 @@
+#include "SampleLocalPlayer.h"
+
---
+++ Samples/Source/Samples/Player/SampleLocalPlayer.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonLocalPlayer.h"
+#include "SampleLocalPlayer.generated.h"
+
+// commonLocalplayer 래핑용 클래스 (원래 lyra에선 기능이 있지만)
+// 일단 클론코딩이라 따라만 생성
+UCLASS()
+class SAMPLES_API USampleLocalPlayer : public UCommonLocalPlayer
+{
+	GENERATED_BODY()
+	
+};
