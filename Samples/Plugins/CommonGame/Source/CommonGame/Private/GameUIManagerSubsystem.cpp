917225c0887ea8d95ac228f0cb8d25223949032e
Hyun <hnjogo@gmail.com>
2025-04-20 10:06:11 +0900

    UGameUIManagerSubsystem Method
    -> 활성화 여부 및 UI 정책 관련 메서드

--- Samples/Plugins/CommonGame/Source/CommonGame/Private/GameUIManagerSubsystem.cpp
+++ Samples/Plugins/CommonGame/Source/CommonGame/Private/GameUIManagerSubsystem.cpp
@@ -1,2 +1,47 @@
-#include "GameUIManagerSubsystem.h"
+﻿#include "GameUIManagerSubsystem.h"
 #include "GameUIPolicy.h"
+
+void UGameUIManagerSubsystem::SwitchToPolicy(UGameUIPolicy* InPolicy)
+{
+	if (CurrentPolicy != InPolicy)
+	{
+		CurrentPolicy = InPolicy;
+	}
+}
+
+void UGameUIManagerSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	// CurrentPolicy가 설정되어 있지 않고, DefaultUIPolicyClass가 제대로 설정된 경우
+	if (!CurrentPolicy && !DefaultUIPolicyClass.IsNull())
+	{
+		// DefaultUIPolicyClass 는 BP 에셋이기에 Load 필요
+		TSubclassOf<UGameUIPolicy> PolicyClass = DefaultUIPolicyClass.LoadSynchronous();
+
+		// NewObject로 인스턴싱 하여 설정
+		SwitchToPolicy(NewObject<UGameUIPolicy>(this, PolicyClass));
+	}
+}
+
+void UGameUIManagerSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+	SwitchToPolicy(nullptr);
+}
+
+bool UGameUIManagerSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	// Dedicate Server의 경우, GameUIManagerSubsystem을 활성화하지 않음
+	// Server에서 UI를 쓸일이 없으니
+	if (!CastChecked<UGameInstance>(Outer)->IsDedicatedServerInstance())
+	{
+		// 어떠한 Subsystem도 UGameUIManagerSubsystem을 상속받지 않았다면,
+		// 해당 GameInstanceSubsystem은 활성화 x
+		TArray<UClass*> ChildClasses;
+		GetDerivedClasses(GetClass(), ChildClasses, false);
+
+		return ChildClasses.Num() == 0;
+	}
+	return false;
+}
--- Samples/Plugins/CommonGame/Source/CommonGame/Public/GameUIManagerSubsystem.h
+++ Samples/Plugins/CommonGame/Source/CommonGame/Public/GameUIManagerSubsystem.h
@@ -10,8 +10,18 @@ UCLASS(Abstract, Config = Game)
 class COMMONGAME_API UGameUIManagerSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-public:
 
+public:
+	// Member Method
+	void SwitchToPolicy(UGameUIPolicy* InPolicy);
+
+public:
+	// UGameInstanceSubsystem Interface
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+public:
 	UPROPERTY(Transient)
 	TObjectPtr<UGameUIPolicy> CurrentPolicy = nullptr;
 
