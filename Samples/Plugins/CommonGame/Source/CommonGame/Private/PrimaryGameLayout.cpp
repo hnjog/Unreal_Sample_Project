1bbba723d108d8c0d27677ce233f6b4ecea51202
Hyun <hnjogo@gmail.com>
2025-04-21 10:06:21 +0900

    최상위 UI에서 Layer 등록 코드 추가

--- Samples/Plugins/CommonGame/Source/CommonGame/Private/PrimaryGameLayout.cpp
+++ Samples/Plugins/CommonGame/Source/CommonGame/Private/PrimaryGameLayout.cpp
@@ -1,2 +1,18 @@
-#include "PrimaryGameLayout.h"
+﻿#include "PrimaryGameLayout.h"
+#include "Widgets/CommonActivatableWidgetContainer.h"
 
+UPrimaryGameLayout::UPrimaryGameLayout(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+void UPrimaryGameLayout::RegisterLayer(FGameplayTag LayerTag, UCommonActivatableWidgetContainerBase* LayerWidget)
+{
+	// bp에서 호출하여 넣어줄 용도
+	if (!IsDesignTime())
+	{
+		// commonUI 에 존재하는 '전환 속도' 설정
+		LayerWidget->SetTransitionDuration(0.0);
+		Layers.Add(LayerTag, LayerWidget);
+	}
+}
--- Samples/Plugins/CommonGame/Source/CommonGame/Public/PrimaryGameLayout.h
+++ Samples/Plugins/CommonGame/Source/CommonGame/Public/PrimaryGameLayout.h
@@ -2,13 +2,29 @@
 
 #include "CoreMinimal.h"
 #include "CommonUserWidget.h"
+#include "GameplayTagContainer.h"
 #include "PrimaryGameLayout.generated.h"
+
+class UCommonActivatableWidgetContainerBase;
 
 UCLASS(Abstract)
 class COMMONGAME_API UPrimaryGameLayout : public UCommonUserWidget
 {
 	GENERATED_BODY()
-	
+public:
+	UPrimaryGameLayout(const FObjectInitializer& ObjectInitializer);
+
+public:
+	// Layer를 추가하며, GameplayTag를 할당
+	// (함수 전달에는 low pointer를 여전히 사용 ->
+	// 별도의 객체 소유권을 '생기지' 않게 하기 위하여)
+	UFUNCTION(BlueprintCallable, Category = "Layer")
+	void RegisterLayer(FGameplayTag LayerTag, UCommonActivatableWidgetContainerBase* LayerWidget);
+
+public:
+	// GameplayTag 와 CommonActivatableWidgetContainerBase 매핑
+	UPROPERTY(Transient, meta = (Categories = "UI.Layer"))
+	TMap<FGameplayTag, TObjectPtr<UCommonActivatableWidgetContainerBase>> Layers;
 };
 
 /*
