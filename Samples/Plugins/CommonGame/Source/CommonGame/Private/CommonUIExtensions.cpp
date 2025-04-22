22704be6446e7303c4b15eb12d992ccffffc9895
Hyun <hnjogo@gmail.com>
2025-04-22 10:19:39 +0900

    UCommonUIExtensions - PushContentToLayer_ForPlayer

--- Samples/Plugins/CommonGame/Source/CommonGame/Private/CommonUIExtensions.cpp
+++ Samples/Plugins/CommonGame/Source/CommonGame/Private/CommonUIExtensions.cpp
@@ -1,2 +1,27 @@
-#include "CommonUIExtensions.h"
+﻿#include "CommonUIExtensions.h"
+#include "CommonActivatableWidget.h"
+#include "GameUIManagerSubsystem.h"
+#include "CommonLocalPlayer.h"
+#include "GameUIPolicy.h"
+#include "PrimaryGameLayout.h"
 
+UCommonActivatableWidget* UCommonUIExtensions::PushContentToLayer_ForPlayer(const ULocalPlayer* LocalPlayer, FGameplayTag LayerName, TSubclassOf<UCommonActivatableWidget> WidgetClass)
+{
+	// LocalPlayer -> GameUISubsystem 가져옴
+	if (UGameUIManagerSubsystem* UIManager = LocalPlayer->GetGameInstance()->GetSubsystem<UGameUIManagerSubsystem>())
+	{
+		// UIManager에서 현재 활성화된 UIPolicy를 가져옴
+		if (UGameUIPolicy* Policy = UIManager->GetCurrentUIPolicy())
+		{
+			// policy에서 Localplayer에 맞는 PrimaryGameLayout을 가져옴
+			if (UPrimaryGameLayout* RootLayout = Policy->GetRootLayout(CastChecked<UCommonLocalPlayer>(LocalPlayer)))
+			{
+				// PrimaryGameLayout, W_OverallUILayout의
+				// LayerName에 Stack으로 WidgetClass(UCommonActivatableWidget)을 붙여줌
+				return RootLayout->PushWidgetToLayerStack(LayerName, WidgetClass);
+			}
+		}
+	}
+
+	return nullptr;
+}
--- Samples/Plugins/CommonGame/Source/CommonGame/Public/CommonUIExtensions.h
+++ Samples/Plugins/CommonGame/Source/CommonGame/Public/CommonUIExtensions.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "CommonUIExtensions.generated.h"
+
+class UCommonActivatableWidget;
+class ULocalPlayer;
 
 UCLASS()
 class COMMONGAME_API UCommonUIExtensions : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+public:
+	UCommonUIExtensions() {}
+
+	static UCommonActivatableWidget* PushContentToLayer_ForPlayer(const ULocalPlayer* LocalPlayer, FGameplayTag LayerName, TSubclassOf<UCommonActivatableWidget> WidgetClass);
 };
