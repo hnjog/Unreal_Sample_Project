1a8c5225b8a5edb69ac348ef30f2cf330dbfc852
Hyun <hnjogo@gmail.com>
2025-04-25 13:47:47 +0900

    LocalPlayer- playerState 관련 Delegate, method

--- Samples/Plugins/CommonGame/Source/CommonGame/Private/CommonLocalPlayer.cpp
+++ Samples/Plugins/CommonGame/Source/CommonGame/Private/CommonLocalPlayer.cpp
@@ -1,2 +1,16 @@
-#include "CommonLocalPlayer.h"
+﻿#include "CommonLocalPlayer.h"
 
+FDelegateHandle UCommonLocalPlayer::CallAndRegister_OnPlayerStateSet(FPlayerStateSetDelegate::FDelegate Delegate)
+{
+	APlayerController* PC = GetPlayerController(GetWorld());
+	APlayerState* PlayerState = PC ? PC->PlayerState : nullptr;
+
+	if (PlayerState)
+	{
+		// player stste가 설정되어 있다면 바로 호출
+		Delegate.Execute(this, PlayerState);
+	}
+
+	// OnPlayerStateSet 등록
+	return OnPlayerStateSet.Add(Delegate);
+}
--- Samples/Plugins/CommonGame/Source/CommonGame/Public/CommonLocalPlayer.h
+++ Samples/Plugins/CommonGame/Source/CommonGame/Public/CommonLocalPlayer.h
@@ -12,4 +12,10 @@ public:
 	// player controller가 local player에 할당(assign) 되었을 경우 실행할 Delegate
 	DECLARE_MULTICAST_DELEGATE_TwoParams(FPlayerControllerSetDelegate,UCommonLocalPlayer* LocalPlayer,APlayerController* PlayerController)
 	FPlayerControllerSetDelegate OnPlayerControllerSet;
+
+	// player state가 local player에 할당(assign) 되었을 경우 실행할 Delegate
+	DECLARE_MULTICAST_DELEGATE_TwoParams(FPlayerStateSetDelegate, UCommonLocalPlayer* LocalPlayer, APlayerState* PlayerState);
+	FPlayerStateSetDelegate OnPlayerStateSet;
+
+	FDelegateHandle CallAndRegister_OnPlayerStateSet(FPlayerStateSetDelegate::FDelegate Delegate);
 };
