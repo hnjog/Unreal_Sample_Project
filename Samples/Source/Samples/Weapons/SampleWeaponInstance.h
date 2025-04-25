3b7f2a61886d7c7af7317e8e9a6a6ba2fca4b968
Hyun <hnjogo@gmail.com>
2025-04-25 16:02:26 +0900

    USampleWeaponInstance - Instigator(활성한 객체) 반환용 함수

--- Samples/Source/Samples/Weapons/SampleWeaponInstance.h
+++ Samples/Source/Samples/Weapons/SampleWeaponInstance.h
@@ -15,6 +15,9 @@ public:
 	// Weapon에 적용할 AnimLayer를 선택하여 반환
 	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = Animation)
 	TSubclassOf<UAnimInstance> PickBestAnimLayer(bool bEquipped, const FGameplayTagContainer& CosmeticTags) const;
+
+	UFUNCTION(BlueprintPure, Category = Equipment)
+	UObject* GetInstigator() const { return Instigator; }
 public:
 	// Weapon에 Equip / UnEquip에 대한 Animation Set 정보
 	// 각각 장착과 해제의 애니메이션을 관리 (모든 무기라도 장착과 해제가 있으므로)
