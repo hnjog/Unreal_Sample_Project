3cbcd3557411928ac716840c51ff94ace5205c64
Hyun <hnjogo@gmail.com>
2025-04-25 16:35:01 +0900

    Reticle용 Fragment 클래스 생성

---
+++ Samples/Source/Samples/Weapons/SampleInventoryFragment_ReticleConfig.cpp
@@ -0,0 +1,1 @@
+#include "SampleInventoryFragment_ReticleConfig.h"
---
+++ Samples/Source/Samples/Weapons/SampleInventoryFragment_ReticleConfig.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "Containers/Array.h"
+#include "Templates/SubclassOf.h"
+#include "Samples/Inventory/SampleInventoryItemDefinition.h"
+#include "SampleInventoryFragment_ReticleConfig.generated.h"
+
+class USampleReticleWidgetBase;
+
+UCLASS()
+class USampleInventoryFragment_ReticleConfig : public USampleInventoryItemFragment
+{
+	GENERATED_BODY()
+public:
+	// 무기에 결합된 ReticleWidget 정보를 가지고 있는 Fragment
+	
+};
