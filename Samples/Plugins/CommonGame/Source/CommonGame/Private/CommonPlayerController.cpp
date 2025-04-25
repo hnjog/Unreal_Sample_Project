1247e1f268cd99d8b2e3d10fdc346be030b86a95
Hyun <hnjogo@gmail.com>
2025-04-25 13:49:56 +0900

    ReceivedPlayer 호출 시, Delegate에 Broadcast 하도록

--- Samples/Plugins/CommonGame/Source/CommonGame/Private/CommonPlayerController.cpp
+++ Samples/Plugins/CommonGame/Source/CommonGame/Private/CommonPlayerController.cpp
@@ -9,6 +9,10 @@ void ACommonPlayerController::ReceivedPlayer()
 	if (UCommonLocalPlayer* LocalPlayer = Cast<UCommonLocalPlayer>(Player))
 	{
 		LocalPlayer->OnPlayerControllerSet.Broadcast(LocalPlayer, this);
+		if (PlayerState)
+		{
+			LocalPlayer->OnPlayerStateSet.Broadcast(LocalPlayer, PlayerState);
+		}
 	}
 
 }
