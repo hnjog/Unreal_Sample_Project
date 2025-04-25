755f706ac1fa44cc184042c8e45eff1f717d536d
Hyun <hnjogo@gmail.com>
2025-04-25 16:02:50 +0900

    weapon용 UI 클래스

---
+++ Samples/Source/Samples/UI/Weapons/SampleWeaponUserInterface.cpp
@@ -0,0 +1,32 @@
+﻿#include "SampleWeaponUserInterface.h"
+#include "Samples/Weapons/SampleWeaponInstance.h"
+#include "Samples/Equipment/SampleEquipmentManagerComponent.h"
+
+USampleWeaponUserInterface::USampleWeaponUserInterface(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+void USampleWeaponUserInterface::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	// Pawn 가져옴
+	if (APawn* Pawn = GetOwningPlayerPawn())
+	{
+		// EquipmentManagerComponent 활용하여, WeaponInstance 가져옴
+		if (USampleEquipmentManagerComponent* EquipmentManager = Pawn->FindComponentByClass<USampleEquipmentManagerComponent>())
+		{
+			if (USampleWeaponInstance* NewInstance = EquipmentManager->GetFirstInstanceOfType<USampleWeaponInstance>())
+			{
+				if (NewInstance != CurrentInstance && NewInstance->GetInstigator() != nullptr)
+				{
+					// 새로 업데이트 및 OnWeaponChanged 호출
+					USampleWeaponInstance* OldWeapon = CurrentInstance;
+					CurrentInstance = NewInstance;
+					OnWeaponChanged(OldWeapon, CurrentInstance);
+				}
+			}
+		}
+	}
+}
---
+++ Samples/Source/Samples/UI/Weapons/SampleWeaponUserInterface.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "SampleWeaponUserInterface.generated.h"
+
+class USampleWeaponInstance;
+
+UCLASS()
+class SAMPLES_API USampleWeaponUserInterface : public UCommonUserWidget
+{
+	GENERATED_BODY()
+public:
+	USampleWeaponUserInterface(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	// Weapon변경에 따른 bp 이벤트
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnWeaponChanged(USampleWeaponInstance* OldWeapon, USampleWeaponInstance* NewWeapon);
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+public:
+	// 현재 장착된 WeaponInstance 추적 (NativeTick 활용하여 업데이트)
+	// 사실 Delegate가 더 효율적
+	UPROPERTY(Transient)
+	TObjectPtr<USampleWeaponInstance> CurrentInstance;
+};
