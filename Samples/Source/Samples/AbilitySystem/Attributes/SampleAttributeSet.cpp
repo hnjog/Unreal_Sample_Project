9dc082236ba10a933b7e4406fc04d6e5f2294198
Hyun <hnjogo@gmail.com>
2025-04-27 14:56:32 +0900

    AttributeSet class 생성
    Attribute?
    - Unreal에서 제공하는 '스텟'과 같은 특성을 제공하는 클래스이며, 일부 상속 구조 등에서 유용하게 사용 가능
    ex :캐릭터
    - 기사, 마법사
    - 마법기사(얘는 둘다 상속받아야 하나?)
    => Attribute로 해당 속성을 '따로' 잡고
         각 클래스들이 필요한 만큼 가져가도록 한다
    (HP Attribute, MP Attribute, Shiled Attribute 등)

---
+++ Samples/Source/Samples/AbilitySystem/Attributes/SampleAttributeSet.cpp
@@ -0,0 +1,6 @@
+#include "SampleAttributeSet.h"
+
+USampleAttributeSet::USampleAttributeSet()
+	:Super()
+{
+}
---
+++ Samples/Source/Samples/AbilitySystem/Attributes/SampleAttributeSet.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "SampleAttributeSet.generated.h"
+
+// Attribute Set에 Attribute 추가시, 선언 및 정의해야할 메서드에 대한 간략 버전 제공
+// 
+// ATTRIBUTE_ACCESSORS (SampleHealthSet,Health)는
+// 아래의 메서드를 선언 및 정의해줌
+// 
+// static FGameplayAttribute GetSampleHealthSet() {...}
+// float GetHealth() const {...}
+// void SetHealth(float NewVal) {...}
+// void InitHealth(float NewVal) {...}
+
+#define ATTRIBUTE_ACCESSORS(ClassName,PropertyName) \
+ GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName,PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName) \
+
+UCLASS()
+class SAMPLES_API USampleAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+public:
+	USampleAttributeSet();
+};
