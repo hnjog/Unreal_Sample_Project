8e076b5188100b9f55764609af01585d01bd2c95
Hyun <hnjogo@gmail.com>
2025-04-28 10:24:26 +0900

    실제로 heal 기능을 실행할 UGameplayEffectExecutionCalculation 클래스 생성

---
+++ Samples/Source/Samples/AbilitySystem/Execution/SampleHealExecution.cpp
@@ -0,0 +1,10 @@
+#include "SampleHealExecution.h"
+
+USampleHealExecution::USampleHealExecution()
+	:Super()
+{
+}
+
+void USampleHealExecution::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+}
---
+++ Samples/Source/Samples/AbilitySystem/Execution/SampleHealExecution.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "SampleHealExecution.generated.h"
+
+/*
+	EGameplayEffectDurationType
+	- Instant, Infinite, HasDuration
+	-> 즉각, 영구, 기간
+	(데미지,패시브, 버프/디버프 등에 대한 다양한 처리에 대한 지원)
+
+	G Component 등으로(5.4 이상)
+	세부 적인 처리가 더 가능하다고 하는데...
+*/
+
+UCLASS()
+class SAMPLES_API USampleHealExecution : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+public:
+	USampleHealExecution();
+
+	// UGameplayEffectExecutionCalculation의 Execute() BlueprintNativeEvent를 override
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+
+};
