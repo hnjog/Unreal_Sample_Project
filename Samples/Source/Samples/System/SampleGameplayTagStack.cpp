63040de8f15a61d4a3e76ffd4429558eef8c3a23
Hyun <hnjogo@gmail.com>
2025-04-29 15:00:55 +0900

    gameplaytagstack 구조체 생성

---
+++ Samples/Source/Samples/System/SampleGameplayTagStack.cpp
@@ -0,0 +1,63 @@
+﻿#include "SampleGameplayTagStack.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(SampleGameplayTagStack)
+
+void FSampleGameplayTagStackContainer::AddStack(FGameplayTag Tag, int32 StackCount)
+{
+	if (!Tag.IsValid())
+		return;
+
+	if (StackCount > 0)
+	{
+		// 선형 순회
+		// Tag가 많지 않으므로 이런 방식인지?
+		// 많아진다면 Map으로 관리하는것이 더 효율적일듯 (10개 미만인 경우는 Array가 더 빠를수도)
+		// 
+		// lyra 에선 전반적인 데이터 개수가 적으면 Array로 만드는 경향이 있음
+		// (애초에 Map의 접근은 o(1)이지만, 상황에 따라 다를수도 있고, 삽입
+		// 삭제의 비용이 더 클 수 있으므로)
+		for (FSampleGameplayTagStack& Stack : Stacks)
+		{
+			if (Stack.Tag == Tag)
+			{
+				const int32 NewCount = Stack.StackCount + StackCount;
+				Stack.StackCount = NewCount;
+				TagToCountMap[Tag] = NewCount;
+				return;
+			}
+		}
+
+		// 새로운 것
+		FSampleGameplayTagStack& NewStack = Stacks.Emplace_GetRef(Tag, StackCount);
+
+		TagToCountMap.Add(Tag, StackCount);
+	}
+}
+
+void FSampleGameplayTagStackContainer::RemoveStack(FGameplayTag Tag, int32 StackCount)
+{
+	if (!Tag.IsValid())
+		return;
+
+	if (StackCount > 0)
+	{
+		for (auto It = Stacks.CreateIterator(); It; It++)
+		{
+			FSampleGameplayTagStack& Stack = *It;
+			if (Stack.StackCount <= StackCount)
+			{
+				// 아래에서 return 하니까 그냥 바로 지워버리는 것
+				// 원래는 return 없었으면 It 새로 지정해야 한다
+				// Unreal에선 RemoveCurrent가 내부적으로 index를 하나 줄여준다
+				It.RemoveCurrent(); 
+				TagToCountMap.Remove(Tag);
+			}
+			else
+			{
+				const int32 NewCount = Stack.StackCount - StackCount;
+				Stack.StackCount = NewCount;
+				TagToCountMap[Tag] = NewCount;
+			}
+			return;
+		}
+	}
+}
---
+++ Samples/Source/Samples/System/SampleGameplayTagStack.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include <GameplayTagContainer.h>
+#include "SampleGameplayTagStack.generated.h"
+
+// 무언가를 대표하는 gameplay tag + 그 개수 count
+USTRUCT(BlueprintType)
+struct FSampleGameplayTagStack
+{
+	GENERATED_BODY()
+public:
+	FSampleGameplayTagStack() {}
+	FSampleGameplayTagStack(FGameplayTag InTag, int32 InStackCount)
+		:Tag(InTag),
+		StackCount(InStackCount)
+	{}
+
+public:
+	UPROPERTY()
+	FGameplayTag Tag;
+
+	UPROPERTY()
+	int32 StackCount = 0;
+};
+
+/*
+	lyra는 Item Base의 시스템 구조
+
+	위 구조체의 Container
+	이걸 다양한 스탯 관리에 사용 가능
+*/
+
+USTRUCT(BlueprintType)
+struct FSampleGameplayTagStackContainer
+{
+	GENERATED_BODY()
+public:
+	FSampleGameplayTagStackContainer() {}
+
+	void AddStack(FGameplayTag Tag, int32 StackCount);
+	void RemoveStack(FGameplayTag Tag, int32 StackCount);
+
+	int32 GetStackCount(FGameplayTag Tag) const
+	{
+		return TagToCountMap.FindRef(Tag);
+	}
+
+	bool ContainsTag(FGameplayTag Tag) const
+	{
+		return TagToCountMap.Contains(Tag);
+	}
+public:
+	UPROPERTY()
+	TArray<FSampleGameplayTagStack> Stacks;
+
+	// Lookup Table (array 방식은 순회를 해야하기에)
+	// 일부 요소에 대한 '개수' 만 필요한 경우에 대하여 더 빠르게 사용할 수 있도록 
+	// 추가로 저장
+	TMap<FGameplayTag, int32> TagToCountMap;
+};
