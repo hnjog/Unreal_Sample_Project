14f9b76891449e3d0db137b839a036c9562d3baf
Hyun <hnjogo@gmail.com>
2025-04-29 17:53:03 +0900

    USampleInventoryFragment_SetStats 가 생성될 때, Instance에 자신의 Stat 적용

--- Samples/Source/Samples/Inventory/SampleInventoryFragment_SetStats.cpp
+++ Samples/Source/Samples/Inventory/SampleInventoryFragment_SetStats.cpp
@@ -1,5 +1,10 @@
 #include "SampleInventoryFragment_SetStats.h"
+#include "SampleInventoryItemInstance.h"
 
 void USampleInventoryFragment_SetStats::OnInstanceCreated(USampleInventoryItemInstance* Instance) const
 {
+	for (const auto& InitialItemStat : InitialItemStats)
+	{
+		Instance->AddStatTagStack(InitialItemStat.Key, InitialItemStat.Value);
+	}
 }
