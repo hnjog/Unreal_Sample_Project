289d390c3669ddfe676b1b31b43a452764d938d8
Hyun <hnjogo@gmail.com>
2025-04-29 16:17:55 +0900

    아이템 Stat 용 속성(Fragment) 정의
    GameplayTag를 통해 주어질 예정

---
+++ Samples/Source/Samples/Inventory/SampleInventoryFragment_SetStats.cpp
@@ -0,0 +1,5 @@
+#include "SampleInventoryFragment_SetStats.h"
+
+void USampleInventoryFragment_SetStats::OnInstanceCreated(USampleInventoryItemInstance* Instance) const
+{
+}
---
+++ Samples/Source/Samples/Inventory/SampleInventoryFragment_SetStats.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SampleInventoryItemDefinition.h"
+#include "GameplayTagContainer.h"
+#include "SampleInventoryFragment_SetStats.generated.h"
+
+UCLASS()
+class SAMPLES_API USampleInventoryFragment_SetStats : public USampleInventoryItemFragment
+{
+	GENERATED_BODY()
+public:
+
+	virtual void OnInstanceCreated(USampleInventoryItemInstance* Instance) const override;
+public:
+	// GameplayTagStackContainer를 통해
+	// 주어질 아이템의 stat
+	UPROPERTY(EditDefaultsOnly, Category = Equipment)
+	TMap<FGameplayTag, int32> InitialItemStats;
+};
